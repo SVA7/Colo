b50f69c7649289428091ef05d60bd202829037c2
Volodymyr <slipak.rs6@gmail.com>
2017-09-24 01:34:18 +0300

    first commit

---
+++ Colo/Colo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Colo
+//
+//  Created by SV_A7 on 24.09.17.
+//  Copyright © 2017 Colo.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
